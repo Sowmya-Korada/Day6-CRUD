8b9e609f30ef9c2ea9efd5fc770c9eb19a5a6d4e
Sowmya-Korada <66238781+Sowmya-Korada@users.noreply.github.com>
2021-05-08 11:39:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,98 +1,95 @@
-const mysql = require("mysql");
-const express = require('express');
-const bodyparser = require('body-parser');
 const path = require('path');
+const express = require('express');
 const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const app = express();
+const mysql = require('mysql');
 
-var app = express();
-
-app.use(bodyparser.json());
-
-var mysqlConnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Sowmya@2000.',
-    database: 'EmployeeDB',
-    multipleStatements: true
+const conn = mysql.createConnection({
+    host: "localhost", 
+    user: "root", 
+    password: "Sowmya@2000.", 
+    database: "employeedb"
 });
 
-mysqlConnection.connect((err) => {
-    if (!err)
-        console.log('DB connection succeded.');
-    else
-        console.log('DB connection failed \n Error : ' + JSON.stringify(err, undefined, 2));
+conn.connect(function(err) {
+    if(err) console.log(err);
+    else console.log("Database connected");
 });
 
-//set views file
+//  set views file
 app.set('views',path.join(__dirname,'views'));
-//set view engine
-app.set('view engine','ejs');
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended: false}));
+			
+// set view engine 
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-
-app.listen(3000, () => console.log('Express server is running at port no : 3000'));
-
-//get all employees
-app.get('/', (req, res) => {
-    mysqlConnection.query('SELECT * FROM Employee', (err, rows, fields) => {
-        if (!err)
-            res.render('employee_index',{
-                title : 'CRUD Operation using NodeJS / Express / MySQL',
-                employee : rows
-            });
-        else
-            console.log(err);
-            
-    })
-});
-
-app.get('/add',(req,res) => {
-    //res.send('new form creation')
-    res.render('add_employee',{
-     title : 'CRUD Operation using NodeJS / Express / MySQL'
-   });
- });
-
-app.post('/save',(req,res) => {
-    let data = {EmpID:req.body.id ,Name: req.body.name, EmpCode: req.body.text, Salary: req.body.salary};
-    let sql = "INSERT INTO employee SET ?";
-    let query = mysqlconnection.query(sql,data,(err,results) =>{
+app.get('/',(req, res) => {
+    // res.send('CRUD Operation using NodeJS / ExpressJS / MySQL');
+    let sql = "SELECT * FROM employee";
+    let query = conn.query(sql, (err, rows) => {
         if(err) throw err;
-        res.redirect('/')
+        res.render('employee_index', {
+            title : 'CRUD Operation using NodeJS / ExpressJS / MySQL',
+            employee : rows
+        });
     });
 });
 
-app.get('/edit/:EmpID',(req,res)=>{
-    const ID = req.params.EmpID;
-    console.log(ID)
-    let sql = `Select * from employee where EmpID = ${ID}`;
-    let query = mysqlconnection.query(sql,(err,result) => {
-        if(err) throw err;
-        res.render('employee_edit',{
-            title: 'CRUD Operation using NodeJs / ExpressJs / MySQL',
-            employee: result[0]
-        })
-    })
-})
+app.get('/add',(req, res)=>{
+    res.render('add_employee',{
+        title:'CRUD Operation using NodeJS / ExpressJS / MySQL'
+    });
+});
 
-app.post('/update',(req,res) => {
-    const ID = req.body.id;
-    let sql = "update employee SET Name='"+req.body.name+"',  EmpCode='"+req.body.text+"',  Salary='"+req.body.salary+"' where EmpID ="+ID;
-    let query = mysqlconnection.query(sql,(err,results) =>{
-        if(err) throw err;
-        res.redirect('/')
+app.post('/save',(req, res)=>{
+    let data= {Name:req.body.Name, EmpCode:req.body.EmpCode, Salary:req.body.Salary};
+    let sql="INSERT INTO employee SET ?";
+    let query= conn.query(sql,data,(err, results)=>{
+        if(err)
+            throw err;
+        res.redirect('/');
+    });
+});
+
+app.get('/edit/:EmpID',(req, res)=>{
+    const empId = req.params.EmpID;
+    let sql=`SELECT * from employee where EmpID= ${empId}`;
+    let query= conn.query(sql,(err,result)=>{
+        if(err) 
+            throw err;
+        res.render('employee_edit',{
+            title:'CRUD Operation using NodeJS / ExpressJS / MySQL',
+            employee: result[0]
+        });
+    });
+});
+
+app.post('/update',(req, res)=>{
+    const id = req.body.EmpID;
+    let sql=`update employee SET Name='${req.body.Name}', EmpCode='${req.body.EmpCode}', Salary='${req.body.Salary}' where EmpID=`+id;
+    let query= conn.query(sql,(err,results)=>{
+        if(err)
+            throw err;
+        res.redirect('/');
     });
 });
 
 app.get('/delete/:EmpID',(req,res)=>{
-    const ID = req.params.EmpID;
-    let sql = `DELETE from employee where EmpID = ${ID}`;
-    let query = mysqlconnection.query(sql,(err,result) => {
-        if(err) throw err;
-        res.redirect('/')
-    })
-})
+    const empId=req.params.EmpID;
+    let sql=`DELETE  from employee where EmpID= ${empId}`;
+    let query= conn.query(sql,(err, result)=>{
+        if(err) 
+            throw err;
+        res.redirect('/');
+        });
+    });
 
+
+//server listening
+app.listen(3000, () => {
+    console.log('Server is running at port 3000');
+});
 
 
